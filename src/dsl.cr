aec35241f99bb3ed987d23123d0465c999a303c8
Jonne Haß <jonne.hass@offerista.com>
2020-02-18 09:38:40 +0100

    Replace removed URI encoding methods with the  new  ones
    For Crystal 0.33 compat

--- src/dsl.cr
+++ src/dsl.cr
@@ -87,7 +87,7 @@ module Artanis
     end
 
     macro call_action(method_name)
-      if %m = URI.unescape(request.path || "/").match({{@type}}::{{ method_name.upcase.id }})
+      if %m = URI.decode(request.path || "/").match({{@type}}::{{ method_name.upcase.id }})
         %ret = {{ method_name.id }}(%m)
 
         {% if method_name.starts_with?("match_") %}
